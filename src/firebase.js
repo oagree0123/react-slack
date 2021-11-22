38953a544991a9bdf6b196e01bff94e6c0c3ee7b
yejun <oagree0123@naver.com>
2021-11-22 23:57:57 +0900

    🎉 Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYBU05SFRcap4a6trtxEK0GbQx5T4tB_M",
+  authDomain: "react-slack-54f6d.firebaseapp.com",
+  projectId: "react-slack-54f6d",
+  storageBucket: "react-slack-54f6d.appspot.com",
+  messagingSenderId: "284286039870",
+  appId: "1:284286039870:web:54d5c659ecfc7f0ab41e6f"
+};
+
